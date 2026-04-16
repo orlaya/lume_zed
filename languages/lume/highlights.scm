ee60f1089bc421d651341ae65042683e9b42a48a
Sarah Hawkless <24264669+orlaya@users.noreply.github.com>
2026-04-16 13:51:11 +1200

    update extension

---
+++ languages/lume/highlights.scm
@@ -0,0 +1,138 @@
+
+; Comments
+; ————————————————————————
+(comment) @comment
+
+
+;
+;
+; Keywords
+; ————————————————————————
+"enum" @keyword
+"struct" @keyword
+"maybe" @keyword
+
+
+;
+;
+; Declarations
+; ————————————————————————
+
+(enum_declaration
+  name: (type_identifier) @type)
+
+(struct_declaration
+  name: (type_identifier) @type)
+
+
+;
+;
+; Variants
+; ————————————————————————
+
+(variant
+  "|" @punctuation.delimiter
+  name: (type_identifier) @constructor)
+
+
+;
+;
+; Fields
+; ————————————————————————
+
+(struct_field
+  name: (identifier) @property
+  ":" @punctuation.delimiter)
+
+(named_field
+  name: (identifier) @property
+  ":" @punctuation.delimiter)
+
+
+;
+;
+; Types
+; ————————————————————————
+
+(generic_type
+  name: (type_identifier) @type)
+
+(maybe_type
+  (type_identifier) @type)
+
+(type_arguments
+  (type_identifier) @type)
+
+(nested_struct
+  (struct_field
+    type: (type_identifier) @type))
+
+(variant_params
+  (type_identifier) @type)
+
+(struct_field
+  type: (type_identifier) @type)
+
+(named_field
+  type: (type_identifier) @type)
+
+
+;
+;
+; Spread
+; ————————————————————————
+
+(spread_type
+  "..." @operator
+  (type_identifier) @type)
+
+
+;
+;
+; Attributes
+; ————————————————————————
+
+(attribute
+  "#" @punctuation.special
+  (identifier) @attribute)
+
+(attribute
+  "(" @punctuation.bracket
+  ")" @punctuation.bracket)
+
+
+;
+;
+; Strings
+; ————————————————————————
+
+(string) @string
+
+
+;
+;
+; Punctuation
+; ————————————————————————
+
+(enum_declaration
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+
+(struct_declaration
+  (struct_body
+    "{" @punctuation.bracket
+    "}" @punctuation.bracket))
+
+(nested_struct
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+
+(variant_params
+  "(" @punctuation.bracket
+  ")" @punctuation.bracket)
+
+(type_arguments
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
+
+"," @punctuation.delimiter
