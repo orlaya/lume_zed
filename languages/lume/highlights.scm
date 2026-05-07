417f644b1320504d27f722ce480ae91f2e6b144c
Sarah Hawkless <24264669+orlaya@users.noreply.github.com>
2026-05-07 12:33:33 +1200

    nope, attributes it is for aka & default

--- languages/lume/highlights.scm
+++ languages/lume/highlights.scm
@@ -150,12 +150,7 @@
 
 (attribute "#" @operator)
 (attribute_name) @attribute
-
-(builtin_attribute
-  (builtin_attribute_name) @keyword)
-
-(attribute
-  (attribute_name) @attribute)
+(builtin_attribute_name) @attribute
 
 (attribute_arguments
   "(" @punctuation.bracket
